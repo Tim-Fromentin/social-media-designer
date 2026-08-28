516202ad2627e779477c65233c6cff425c13d79b
Tim <tim.fromentin.dev.pro@gmail.com>
2026-08-28 22:00:22 +0200

    feat: add new design

--- testimonial/front/app/src/components/Testimonial.tsx
+++ testimonial/front/app/src/components/Testimonial.tsx
@@ -10,7 +10,7 @@ interface Entry {
 export function Testimonial({ name, avatar, message, note }: Entry) {
   return (
     <div className="flex-1 bg-primary-400 h-full flex items-center justify-center p-8 overflow-auto">
-      <div className="max-w-md w-full bg-linear-to-br from-purple-600 to-blue-500 rounded-2xl shadow-2xl overflow-hidden transform hover:scale-105 transition-transform duration-300">
+      {/* <div className="max-w-md w-full bg-linear-to-br from-purple-600 to-blue-500 rounded-2xl shadow-2xl overflow-hidden transform hover:scale-105 transition-transform duration-300">
         <div className="p-6 sm:p-8">
           <div className="flex justify-between items-center mb-4">
             <div className="flex items-center space-x-2">
@@ -40,6 +40,30 @@ export function Testimonial({ name, avatar, message, note }: Entry) {
               </p>
             </div>
           </div>
+        </div>
+      </div> */}
+
+      <div className="max-w-md w-full bg-white rounded-sm p-4">
+        <div className="flex items-center space-x-2 mb-5">
+          {[...Array(note || 0).keys()].map((index) => (
+            <Star
+              key={index}
+              className="w-5 h-5 fill-amber-400 text-amber-400"
+            />
+          ))}
+          {[...Array(5 - (note || 0)).keys()].map((index) => (
+            <Star key={index} className="w-5 h-5 fill-gray-300 text-gray-300" />
+          ))}
+        </div>
+        <p>{message}</p>
+
+        <div className="flex items-center gap-2 mt-5">
+          <img
+            src={avatar}
+            alt={avatar}
+            className="w-12 h-12 rounded-full border-2 border-white"
+          />
+          <p className="text-gray-500 font-semibold">{name || "Sarah Johnson"}</p>
         </div>
       </div>
     </div>
