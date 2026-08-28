1cff059becfbd49ac997189d6831b9becf78e0e3
Tim <tim.fromentin.dev.pro@gmail.com>
2026-08-28 21:49:41 +0200

    feat: change avatar, message, name and note value

--- testimonial/front/app/src/App.tsx
+++ testimonial/front/app/src/App.tsx
@@ -1,13 +1,13 @@
-import { useState, type SetStateAction } from "react";
+import { useState } from "react";
 import { SideBar } from "./components/Sidebar";
 import { Testimonial } from "./components/Testimonial";
 
 export default function App() {
   const [name, setName] = useState<string>("John Doe");
   const [avatar, setAvatar] = useState<string>(
-    "https://api.dicebear.com/10.x/lorelei/svg?seed=Felix",
+    "https://randomuser.me/api/portraits/women/91.jpg",
   );
-  const [note, setNote] = useState<number>(0);
+  const [note, setNote] = useState<number>(1);
   const [message, setMessage] = useState<string>(
     "Lorem ipsum dolor sit amet. Qui doloribus praesentium ex velit atque non unde facilis non asperiores dolor. Qui harum placeat et cumque quisquam quo perferendis soluta cum nihil suscipit.",
   );
@@ -19,7 +19,10 @@ export default function App() {
         avatar={avatar}
         setAvatar={setAvatar}
         message={message}
-        setMessage={setMessage} note={note} setNote={setNote}      />
+        setMessage={setMessage}
+        note={note}
+        setNote={setNote}
+      />
       <Testimonial name={name} avatar={avatar} message={message} note={note} />
     </main>
   );
--- testimonial/front/app/src/components/Sidebar.tsx
+++ testimonial/front/app/src/components/Sidebar.tsx
@@ -19,13 +19,12 @@ export function SideBar({
   message,
   setMessage,
   note,
-  setNote
+  setNote,
 }: Entry) {
   return (
     <aside className="w-96 flex flex-col gap-4 py-8 px-6 bg-white border-r border-gray-100 h-screen shrink-0 overflow-y-auto">
       <div>
         <h1 className="text-sm font-bold text-primary-950">
-          {name}
           Social Design Suite
         </h1>
         <p className="text-xs text-gray-500">Génerateur de testimonial</p>
@@ -47,6 +46,8 @@ export function SideBar({
             name="name"
             placeholder="John Doe"
             className="w-full py-1.5 px-3 text-sm text-gray-900 focus:outline-none rounded-md"
+            defaultValue={name}
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
       </div>
@@ -65,6 +66,7 @@ export function SideBar({
             name="avatar"
             placeholder="img.com"
             className="w-full py-1.5 px-3 text-sm text-gray-900 focus:outline-none rounded-md"
+            onChange={(e) => setAvatar(e.target.value)}
           />
         </div>
       </div>
@@ -73,12 +75,19 @@ export function SideBar({
         <span className="block text-sm font-medium text-primary-900 mb-1.5">
           Note
         </span>
-        <div className="flex items-center gap-1 py-1 text-gray-300">
-          <Star className="w-5 h-5 fill-amber-400 text-amber-400 cursor-pointer" />
-          <Star className="w-5 h-5 cursor-pointer hover:text-amber-400" />
-          <Star className="w-5 h-5 cursor-pointer hover:text-amber-400" />
-          <Star className="w-5 h-5 cursor-pointer hover:text-amber-400" />
-          <Star className="w-5 h-5 cursor-pointer hover:text-amber-400" />
+        <div className="flex items-center rounded-md bg-white border focus-within:border-primary-400 focus-within:ring-1 focus-within:ring-primary-400">
+          <input
+            id="avatar"
+            type="number"
+            min={0}
+            max={5}
+            step={1}
+            name="avatar"
+            defaultValue={1}
+            placeholder="5"
+            className="w-full py-1.5 px-3 text-sm text-gray-900 focus:outline-none rounded-md"
+            onChange={(e) => setNote(parseInt(e.target.value))}
+          />
         </div>
       </div>
 
@@ -96,6 +105,8 @@ export function SideBar({
             rows={3}
             placeholder="Lorem ipsum"
             className="w-full py-1.5 px-3 text-sm text-gray-900 focus:outline-none rounded-md resize-none"
+            onChange={(e) => setMessage(e.target.value)}
+            defaultValue={message}
           />
         </div>
       </div>
